322e8828d565ffa9e721efaf01db6ee8e2345878
Arushi3154 <arushiawasthi2004@gmail.com>
2026-07-13 17:38:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.1 MB (88.12%)

--- my-workspace/0027-remove-element/solution.py
+++ my-workspace/0027-remove-element/solution.py
@@ -1,16 +1,11 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        # This pointer tracks the next position to write a non-val element
         k = 0
         
-        # Iterate through the entire array
         for i in range(len(nums)):
-            # If the current element is not the target value
             if nums[i] != val:
-                # Move it to the front at index k
                 nums[k] = nums[i]
-                # Advance the k pointer
                 k += 1
                 
-        # k represents the total number of elements not equal to val
+       
         return k
